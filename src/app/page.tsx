52899ddd517ebeaf3f2ef5f4f6288aa1c80b464f
hwilliams-camelcase <hunter@camelcasecollective.com>
2025-10-22 13:41:45 -0500

    remove heading styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ function Clients() {
     <div className="mt-24 rounded-4xl bg-neutral-950 py-20 sm:mt-32 sm:py-32 lg:mt-56">
       <Container>
         <FadeIn className="flex items-center gap-x-8">
-          <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
+          <h2 className="text-center text-white">
             Our Highlights
           </h2>
           <div className="h-px flex-auto bg-neutral-800" />
@@ -45,7 +45,7 @@ function Clients() {
         <FadeInStagger faster>
           <ul
             role="list"
-            className="mt-10 grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-4 auto-rows-min"
+            className="mt-10 grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-3"
           >
             {clients.map(([client, Logo]) => (
               <li key={client} className="flex justify-center">
