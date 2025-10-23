39bf8ad988b71e8efaaca07d291dbd7272c7714d
Hayden Cleek <hayden@camelcasecollective.com>
2025-10-23 00:37:16 -0400

    Update text size of testimonial

--- src/components/Blockquote.tsx
+++ src/components/Blockquote.tsx
@@ -55,7 +55,7 @@ function BlockquoteWithoutImage({
 }) {
   return (
     <Border position="left" className={clsx('pl-8', className)}>
-      <figure className="text-sm">
+      <figure className="text-base">
         <blockquote className="text-neutral-600 *:relative [&>:first-child]:before:absolute [&>:first-child]:before:right-full [&>:first-child]:before:content-['“'] [&>:last-child]:after:content-['”']">
           {typeof children === 'string' ? <p>{children}</p> : children}
         </blockquote>
